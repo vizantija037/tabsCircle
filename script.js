b40b575dd901d0b372753d11b1a25bd78afac7cb
vizantija037 <80783335+vizantija037@users.noreply.github.com>
2022-02-09 23:01:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonsContainer = document.querySelector('.buttons-container');
 const buttonElement = document.querySelectorAll('.button');
 const textArea = document.querySelectorAll('.content-container');
+const btnDark = document.getElementById('btn');
+const lightBody = document.querySelector('body');
 
 buttonsContainer.addEventListener('click', function(e){
    const btnId = e.target.dataset.id;
@@ -16,11 +18,20 @@ buttonsContainer.addEventListener('click', function(e){
         item.classList.remove('active');
         const showContent = document.getElementById(btnId);
         console.log(showContent);
-        showContent.classList.add('active');
-       
-        
+        showContent.classList.add('active');        
     })
    }
-   
-   
+})
+
+btnDark.addEventListener('click', function(){
+    btnDark.classList.toggle('active');
+    if(btnDark.innerHTML==='Turn ON the light'){
+        btnDark.innerHTML='Turn OFF the light';
+        lightBody.classList.add('active')
+    } else {
+        btnDark.innerHTML='Turn ON the light';
+        lightBody.classList.remove('active')
+    }
+
+
 })
